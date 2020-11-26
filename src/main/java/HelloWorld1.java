66cd3491ff136ac8229e9587910e70f8ed1dc37e
yuuki-train <yuuki323-sld51@kcf.biglobe.ne.jp>
2020-11-26 16:57:48 +0900

    HelloWorld1 first commit

---
+++ src/main/java/HelloWorld1.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+
+
+public class HelloWorld1{
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+    public static void main(String[] args){
+        SpringApplication.run(HelloWorld1.class, args);
+    }
+
+
+}
